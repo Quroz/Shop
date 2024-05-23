b63a089d376ddc46bea411a6437edcded3bbf277
Quroz <filipwt@hotmail.com>
2024-05-23 17:22:24 +0200

    new

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import "react-native-gesture-handler";
 import Navigator from "./Navigator";
 import { UserProvider } from "./apps/Context/UserContext";
-
+import { ModalPortal } from "react-native-modals";
 export default function App() {
   return (
     <>
       <UserProvider>
         <Navigator />
+        <ModalPortal />
       </UserProvider>
     </>
   );
--- apps/Screens/BottomTabsScreens/TabsHome.jsx
+++ apps/Screens/BottomTabsScreens/TabsHome.jsx
@@ -1,14 +1,55 @@
 import { StyleSheet, Text, View, SafeAreaView, TextInput, TouchableOpacity, FlatList, Image, ScrollView } from 'react-native'
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { UserContext } from '../../Context/UserContext'
 import { AntDesign, Feather, Ionicons, MaterialIcons } from "@expo/vector-icons";
 import { list, images, deals, offers } from "../../../data"
 import { ImageSlider } from "react-native-image-slider-banner";
+import { Dropdown } from 'react-native-element-dropdown';
 
 const TabsHome = () => {
 
     const { userDetails } = useContext(UserContext);
+    const [products, setProducts] = useState([])
+    const [filteredProducts, setFilteredProducts] = useState([])
+    const [value, setValue] = useState(null);
+    const [categories, setCategories] = useState([
+        { label: "All categories", value: "All" },
+        { label: "Men's clothing", value: "men's clothing" },
+        { label: "jewelery", value: "jewelery" },
+        { label: "electronics", value: "electronics" },
+        { label: "women's clothing", value: "women's clothing" },
+    ]);
 
+    useEffect(() => {
+        if (value == null || value === "All" || !categories.some(category => category.value === value)) {
+            setFilteredProducts(products);
+        } else {
+            setFilteredProducts(products.filter(item => item.category === value));
+        }
+    }, [value, products, categories]);
+
+
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch("https://fakestoreapi.com/products", {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                });
+
+                const data = await response.json()
+                setProducts(data)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        fetchData()
+    }, [])
+
+    console.log(filteredProducts)
     return (
         <SafeAreaView style={{ backgroundColor: "white", flex: 1 }}>
             <View style={{
@@ -114,6 +155,62 @@ const TabsHome = () => {
                     />
                 </View>
                 <View style={{ borderColor: "gray", borderWidth: 2, marginTop: 15 }} />
+                <View style={{ padding: 10 }}>
+                    <Dropdown
+                        style={{
+                            height: 50,
+                            borderColor: 'gray',
+                            borderWidth: 0.5,
+                            borderRadius: 8,
+                            paddingHorizontal: 8,
+                            width: "50%"
+                        }}
+                        placeholderStyle={{ fontSize: 16 }}
+                        selectedTextStyle={{ fontSize: 16 }}
+                        inputSearchStyle={{
+                            height: 40,
+                            fontSize: 16
+                        }}
+                        iconStyle={{
+                            width: 20,
+                            height: 20,
+                        }}
+                        data={categories}
+                        search
+                        value={value}
+                        onChange={item => {
+                            setValue(item.value);
+                        }}
+                        maxHeight={300}
+                        labelField="label"
+                        valueField="value"
+                        placeholder={'Select item'}
+                        searchPlaceholder="Search..."
+                        renderLeftIcon={() => (
+                            <AntDesign
+                                style={{ marginRight: 5 }}
+                                name="Safety"
+                                size={20}
+                            />
+                        )}
+                    />
+                </View>
+                <View style={{ flexWrap: "wrap", flexDirection: "row", alignItems: "center", justifyContent: "space-around" }}>
+
+                    {filteredProducts?.map((item, index) => (
+                        <View key={index} style={{ marginVertical: 20 }}>
+                            <Image source={{ uri: item.image }} style={{ width: 150, height: 150 }} />
+                            <Text numberOfLines={1} style={{ width: 150, marginTop: 5 }}>{item.title}</Text>
+                            <View style={{ alignItems: "center", flexDirection: "row", justifyContent: "space-between", marginTop: 5 }}>
+                                <Text style={{ fontWeight: "bold", fontSize: 15 }}>{item.price}</Text>
+                                <Text style={{ fontWeight: "bold", color: "#FFC72C" }}>{item.rating.rate} ratings</Text>
+                            </View>
+                            <TouchableOpacity style={{ backgroundColor: "#FFC72C", borderRadius: 99, paddingHorizontal: 10, paddingVertical: 15, alignItems: "center", justifyContent: "center", marginTop: 5 }}>
+                                <Text style={{ fontSize: 15 }}>Add to Cart</Text>
+                            </TouchableOpacity>
+                        </View>
+                    ))}
+                </View>
             </ScrollView>
         </SafeAreaView>
     )
@@ -121,4 +218,4 @@ const TabsHome = () => {
 
 export default TabsHome
 
-const styles = StyleSheet.create({})
+
